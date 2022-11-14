6f43836e20043325b627670dadcc5f85994afaac
tixy74 <timlab74@gmail.com>
2022-11-14 09:02:45 +0100

    init ms-store

---
+++ MS-Store/ms-store/src/test/java/com/asi2/msstore/MsStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi2.msstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
