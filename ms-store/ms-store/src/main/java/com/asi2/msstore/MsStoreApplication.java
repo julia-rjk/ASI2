7c06a5889e3d2267b0eadef1fa4ff5f60faa4d5b
tixy74 <timlab74@gmail.com>
2022-11-14 16:43:57 +0100

    CardBasic

--- ms-store/ms-store/src/main/java/com/asi2/msstore/MsStoreApplication.java
+++ ms-store/ms-store/src/main/java/com/asi2/msstore/MsStoreApplication.java
@@ -3,7 +3,10 @@ package com.asi2.msstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2 
 public class MsStoreApplication {
 
 	public static void main(String[] args) {
