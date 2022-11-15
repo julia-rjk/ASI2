837186f47d953056d2311c3e72a1b434163fc0ec
tixy74 <timlab74@gmail.com>
2022-11-15 09:43:18 +0100

    Repair card swagger

--- MS-Card/src/main/java/com/asi2/mscard/MsCardApplication.java
+++ MS-Card/src/main/java/com/asi2/mscard/MsCardApplication.java
@@ -2,10 +2,12 @@ package com.asi2.mscard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
-@EnableSwagger2
+@OpenAPIDefinition(info = @Info(title = "Card Rest Api", version = "1.0", description = "Information about the Card Rest API and how to interact with"))
 public class MsCardApplication {
 
     public static void main(String[] args) {
