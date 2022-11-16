60f82b01a16a513dc2f1b8e99751e905faef32f5
Nico-Mugi <nico.thouvenin13@gmail.com>
2022-11-16 14:59:18 +0100

    xd

--- client/src/pages/Game/Game.tsx
+++ client/src/pages/Game/Game.tsx
@@ -2,31 +2,32 @@ import React, { useEffect } from 'react';
 import { Player } from '../../components/Player/Player';
 import { io } from 'socket.io-client';
 import './Game.css';
-import { nodeURL } from '../../api/api';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectUser } from '../../redux/user.selector';
 
 export const Game = () => {
-  const socket = io(nodeURL);
+  const socket = io('http://localhost:8087');
   const user = useSelector(selectUser);
   useEffect(() => {
-    socket.on("connect", () => {
+    socket.on('connect', () => {
       socket.emit('joinWaitingList', { user });
-      console.log('connected')
+      console.log('connected');
     });
-  }, [])
+  }, []);
 
   return (
-    <div id='gameContainer'>
+    <div id="gameContainer">
       <div id="chat">Chat (TODO)</div>
-      <div id='game'>
-        <Player playerName={`${user.lastName} ${user.surName}`} playerActionPoints={100} ></Player>
-        <div id='controls'>
-          <button className='control'>End turn</button>
-          <button className='control'>Attack ⚔️</button>
+      <div id="game">
+        <Player
+          playerName={`${user.lastName} ${user.surName}`}
+          playerActionPoints={100}></Player>
+        <div id="controls">
+          <button className="control">End turn</button>
+          <button className="control">Attack ⚔️</button>
         </div>
         {/* {cond ? <A /> : <B />} */}
-        <Player playerName={'Julia'} playerActionPoints={100} ></Player>
+        <Player playerName={'Julia'} playerActionPoints={100}></Player>
       </div>
     </div>
   );
