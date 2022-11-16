785f5d2422aaa7d621f47983ef1640b157e6495b
tixy74 <timlab74@gmail.com>
2022-11-16 13:48:07 +0100

    socket on client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Layout } from './components/Layout';
+import { Game } from './pages/Game';
 import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { UserForm } from './pages/UserForm';
@@ -12,7 +13,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="store/:type" element={<Store />} />
-        <Route path="fight" element={<div>COMING SOON !!!!!!!</div>} />
+        <Route path="game" element={<Game />} />
       </Route>
       <Route path="/public" element={<UserForm />} />
       <Route path="*" element={<div>Not found</div>} />
---
+++ client/src/pages/Game/Game.tsx
@@ -0,0 +1,39 @@
+import React, { useEffect } from 'react';
+import { Player } from '../../components/Player/Player';
+import { io } from 'socket.io-client';
+import './Game.css';
+import { nodeURL } from '../../api/api';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectUser } from '../../redux/user.selector';
+
+export const Game = () => {
+  const socket = io(nodeURL);
+  const user = useSelector(selectUser);
+  useEffect(() => {
+    socket.on("connect", () => {
+      socket.emit("connected", (res:any) => {
+        console.log('res : ',res);
+        if ("error" in res) {
+          // handle the error
+          return;
+        }
+      })
+      console.log('connected')
+    });
+  }, [])
+
+  return (
+    <div id='gameContainer'>
+      <div id="chat">Chat (TODO)</div>
+      <div id='game'>
+        <Player playerName={`${user.lastName} ${user.surName}`} playerActionPoints={100} ></Player>
+        <div id='controls'>
+          <button className='control'>End turn</button>
+          <button className='control'>Attack ⚔️</button>
+        </div>
+        {/* {cond ? <A /> : <B />} */}
+        <Player playerName={'Julia'} playerActionPoints={100} ></Player>
+      </div>
+    </div>
+  );
+};
