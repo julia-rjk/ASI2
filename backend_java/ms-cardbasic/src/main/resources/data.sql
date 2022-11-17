f3e5b4aeba7d4599af51500e0232bd061de8381e
tixy74 <timlab74@gmail.com>
2022-11-17 21:50:44 +0100

    generic cards

---
+++ backend_java/ms-cardbasic/src/main/resources/data.sql
@@ -0,0 +1,116 @@
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        1,
+        'Bulbasaur',
+        'I am Bulbasaur',
+        'Grass',
+        'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/cards/xy7/1.png'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        2,
+        'GigaCha',
+        'Juste le boss en faite',
+        'Giga',
+        'https://i.kym-cdn.com/entries/icons/original/000/026/152/gigachad.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        3,
+        'Chien',
+        'Whouf whouf',
+        'Animal',
+        'https://jardinage.lemonde.fr/images/dossiers/2022-07/langage-chien-(2)-151744.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        4,
+        'Crocodile',
+        'Je suis un croco',
+        'Animal',
+        'https://www.monbeaujardin.fr/wp-content/uploads/2017/03/crocodile-du-nil-1.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        5,
+        'Girafe',
+        'J aime les feuilles',
+        'Animal',
+        'https://www.worldwildlife.org/content/dam/ WWF/assets/photos/00/imported/WWFBinaryAsset/original/110311_giraffe-2_WCS_9673.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        6,
+        'Panda',
+        'Le banbou c est délicieux',
+        'Animal',
+        'https://www.monbeaujardin.fr/wp-content/uploads/2017/03/crocodile-du-nil-1.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        7,
+        'Lion',
+        'Roar',
+        'Animal',
+        'https://www.monbeaujardin.fr/wp-content/uploads/2017/03/crocodile-du-nil-1.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        8,
+        'Cheval',
+        'Pfff',
+        'Animal',
+        'https://www.monbeaujardin.fr/wp-content/uploads/2017/03/crocodile-du-nil-1.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        9,
+        'Tigre',
+        'Je suis un tigre',
+        'Animal',
+        'https://www.monbeaujardin.fr/wp-content/uploads/2017/03/crocodile-du-nil-1.jpg'
+    );
+
+    
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        10,
+        'Tyrant',
+        'Le boss final',
+        'Tyran',
+        'https://m.media-amazon.com/images/M/MV5BNzY4NTg3MzEzMV5BMl5BanBnXkFtZTgwODU5ODgxOTE@._V1_.jpg'
+    );
+
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        11,
+        'Cantas',
+        'Un monstre',
+        'Cantas',
+        'https://www.researchgate.net/publication/332712533/figure/fig2/AS:781468355788802@1563327983337/Vibration-reduction-of-CANTAS-by-the-vibration-isolator-system.png'
+    );
+    
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        12,
+        'Boule',
+        'Boule',
+        'Ball',
+        'https://img-4.linternaute.com/eFxkRbOaTUwl8NcxHY2oW-v29nQ=/1500x/smart/53d67437f5c3422fac6de2ed824ed823/ccmcms-linternaute/32933456.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        13,
+        'Sphinx',
+        'Sphinx',
+        'Sphinx',
+        'https://cdn.britannica.com/85/99185-050-E1110E5C/Great-Sphinx-Giza-Egypt.jpg'
+    );
+INSERT INTO PUBLIC.CARD_BASICS (ID, NAME, DESCRIPTION, FAMILY, IMG_URL)
+VALUES (
+        14,
+        'Cinq',
+        'Cinq',
+        'Five',
+        'https://cdn2.vectorstock.com/i/1000x1000/10/21/sweet-cake-number-character-five-vector-21531021.jpg'
+    );
