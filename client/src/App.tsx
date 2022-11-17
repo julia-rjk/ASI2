99395da24a0e47305e57e991c9fa04f3b3b4bfa4
Nico-Mugi <nico.thouvenin13@gmail.com>
2022-11-17 14:01:29 +0100

    DEL front atelier 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Layout } from './components/Layout';
-import { Game } from './pages/Game';
 import { Home } from './pages/Home';
 import { Store } from './pages/Store';
 import { UserForm } from './pages/UserForm';
@@ -13,7 +12,7 @@ export const App = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="store/:type" element={<Store />} />
-        <Route path="game" element={<Game/>} />
+        <Route path="game" element={<div> COMINING SOON!!! </div>} />
       </Route>
       <Route path="/public" element={<UserForm />} />
       <Route path="*" element={<div>Not found</div>} />
