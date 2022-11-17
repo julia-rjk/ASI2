34d07f82209e18b7e1a190892ae32a758787a332
charlesboyerCPE <charles.boyer021@gmail.com>
2022-11-17 14:23:23 +0100

    Init MS-LogESB

---
+++ ms-logEsb/src/test/java/com/asi2/mslogesb/MsLogEsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asi2.mslogesb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsLogEsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
