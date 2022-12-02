f3781e72d6834cfdda65cf3412c3a61c9c6e814d
Nico-Mugi <nico.thouvenin13@gmail.com>
2022-12-02 12:11:17 +0100

    toiso

--- backend_nodeJs/ms-game/src/services/chatService.ts
+++ backend_nodeJs/ms-game/src/services/chatService.ts
@@ -16,7 +16,7 @@ export default class ChatService {
 
     public sendMessage(io: Server, msg: MessageDTO) {
         msg = {...msg, date: new Date()};
-        axios.post(URL_MS_CHATHISTORY, msg);
+        axios.post(URL_MS_CHATHISTORY, {msg, date: msg.date?.toISOString()});
         if(msg.room){
             io.to(msg.room).emit('chatMessage', msg);
         }
