e3c79254440ac8c0baef245db6bbb174316f02a8
charlesboyerCPE <charles.boyer021@gmail.com>
2022-12-18 15:11:20 +0100

    Fix annotation repository Store service

--- backend_java/ms-store/ms-store/src/main/java/com/asi2/msstore/repository/StoreDAO.java
+++ backend_java/ms-store/ms-store/src/main/java/com/asi2/msstore/repository/StoreDAO.java
@@ -2,7 +2,9 @@ package com.asi2.msstore.repository;
 
 import com.asi2.msstore.model.entity.StoreTransaction;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StoreDAO extends CrudRepository<StoreTransaction, Integer> {
 
 }
